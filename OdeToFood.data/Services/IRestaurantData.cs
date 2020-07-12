ee77f46d716780edd453ac4c0f3d8548045a2311
hermanusb <hermanusbosman@yahoo.co.za>
2020-07-12 10:32:48 +0200

    Add Start code

---
+++ OdeToFood.data/Services/IRestaurantData.cs
@@ -0,0 +1,14 @@
+﻿using OdeToFood.data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
---
+++ OdeToFood.data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Hermanus's Pizza", Cuisine = CuisineType.Italian },  
+                new Restaurant { Id = 2, Name = "La resistance", Cuisine = CuisineType.French },  
+                new Restaurant { Id = 3, Name = "Papriika", Cuisine = CuisineType.Indian }  
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
